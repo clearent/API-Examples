8a4d34b79b804ba7d3a48e11ed45405a68fa5cbe
carmbruster <carmbruster@clearent.com>
2015-04-30 11:19:59 -0500

    Added code to return messages on failed transactions

--- src/com/clearent/sample/Transaction.java
+++ src/com/clearent/sample/Transaction.java
@@ -10,151 +10,184 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
-public class Transaction{
+public class Transaction {
 
-  private static final String API_URI = "https://gateway-sb.clearent.net/rest/v2/transactions";
-  private static final String API_KEY = "762ad38136b54570a03be604a5a5a1e5";
-  private static final String ACCEPT_HEADER_KEY = "Accept";
-  private static final String APPLICATION_JSON = "application/json";
-  private static final String CONTENT_TYPE_KEY = "Content-Type";
-  private static final String POST_METHOD = "POST";
-  private static final boolean OUTPUT_TRUE = true;
+    private static final String API_URI = "https://gateway-sb.clearent.net/rest/v2/transactions";
+    private static final String API_KEY = "api key goes here";
+    private static final String ACCEPT_HEADER_KEY = "Accept";
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String CONTENT_TYPE_KEY = "Content-Type";
+    private static final String POST_METHOD = "POST";
+    private static final boolean OUTPUT_TRUE = true;
 
-  public static void main(String[] args) throws Exception {
-    String response;
-    // sale
-    response = requestSale();
-    System.out.println(response);
-    // authorization
-    response = requestAuth();
-    System.out.println(response);
-    // capture
-    response = requestCapture();
-    System.out.println(response);
-    // forced sale
-    response = requestForcedSale();
-    System.out.println(response);
-    // refund
-    response = requestRefund();
-    System.out.println(response);
-    // void
-    response = requestVoid();
-    System.out.println(response);
-  }
+    public static void main(String[] args) throws Exception {
+        String response;
 
-  public static String requestSale() throws Exception {
-      final String card = "4111111111111111";
-      final String exp_date = "1219";
-      final String amount = "25.00";
-      final String requestBody = "{\"type\":\"SALE\",\"card\":\"" + card + "\",\"exp-date\":\"" + exp_date + "\",\"amount\":\"" + amount + "\"}";
-      return requestTransaction(requestBody);
-  }
+        // sale --
+        System.out.println("Beginning Sale Request");
+        response = requestSale();
+        System.out.println(response);
 
-  public static String requestAuth() throws Exception {
-      final String card = "4111111111111111";
-      final String exp_date = "1219";
-      final String amount = "25.00";
-      final String requestBody = "{\"type\":\"AUTH\",\"card\":\"" + card + "\",\"exp-date\":\"" + exp_date + "\",\"amount\":\"" + amount + "\"}";
-      return requestTransaction(requestBody);
-  }
+        // authorization --
+        System.out.println("Beginning Authorization Request");
+        response = requestAuth();
+        System.out.println(response);
 
-  public static String requestCapture() throws Exception {
-      final String id = "12345";
-      final String amount = "25.00";
-      final String requestBody = "{\"type\":\"CAPTURE\",\"id\":\"" + id + "\",\"amount\":\"" + amount + "\"}";
-      return requestTransaction(requestBody);
-  }
+        // capture
+        System.out.println("Beginning Capture Request");
+        response = requestCapture();
+        System.out.println(response);
 
-  public static String requestForcedSale() throws Exception {
-      final String card = "4111111111111111";
-      final String exp_date = "1219";
-      final String amount = "25.00";
-      final String order_id = "12345";
-      final String authorization_code = "APPC10";
-      final String requestBody = "{\"type\":\"FORCED SALE\",\"card\":\"" + card + "\",\"exp-date\":\"" + exp_date + "\",\"amount\":\"" + amount + "\",\"order-id\":\"" + order_id + "\",\"authorization-code\":\"" + authorization_code + "\"}";
-      return requestTransaction(requestBody);
-  }
+        // forced sale --
+        System.out.println("Beginning Forced Request");
+        response = requestForcedSale();
+        System.out.println(response);
 
-  public static String requestRefund() throws Exception {
-      final String card = "4111111111111111";
-      final String amount = "25.00";
-      final String requestBody = "{\"type\":\"REFUND\",\"card\":\"" + card + "\",\"amount\":\"" + amount + "\"}";
-      return requestTransaction(requestBody);
-  }
+        // refund
+        System.out.println("Beginning Refund Request");
+        response = requestRefund();
+        System.out.println(response);
 
-  public static String requestVoid() throws Exception {
-      final String id = "12345";
-      final String requestBody = "{\"type\":\"VOID\",\"id\":\"" + id + "\"}";
-      return requestTransaction(requestBody);
-  }
-
-  private static String requestTransaction(String requestBody) throws IOException {
-
-    HttpURLConnection httpConnection = null;
-    try {
-      httpConnection = setupHttpConnection(API_URI);
-      setRequestParamters(httpConnection, API_URI);
-      sendRequest(requestBody, httpConnection.getOutputStream());
-      return getResponse(httpConnection.getInputStream());
-    } finally {
-      if (httpConnection != null) {
-        httpConnection.disconnect();
-      }
+        // void
+        System.out.println("Beginning Void Request");
+        response = requestVoid();
+        System.out.println(response);
     }
-  }
 
-  private static HttpURLConnection setupHttpConnection(final String apiEndpoint) {
-    final URL url = createUrl(apiEndpoint);
-    return openHttpConnection(url);
-  }
-
-  private static HttpURLConnection openHttpConnection(final URL url) {
-    try {
-      final HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
-      httpConnection.setDoOutput(OUTPUT_TRUE);
-      return httpConnection;
-    } catch (IOException ioe) {
-      throw new IllegalStateException("Unable to open connection with endpoint", ioe);
+    public static String requestSale() throws Exception {
+        final String card = "4111111111111111";
+        final String exp_date = "1219";
+        final String amount = "25.00";
+        final String requestBody = "{\"type\":\"SALE\",\"card\":\"" + card
+                + "\",\"exp-date\":\"" + exp_date + "\",\"amount\":\"" + amount
+                + "\"}";
+        return requestTransaction(requestBody);
     }
-  }
 
-  private static URL createUrl(final String apiEndpoint) {
-    try {
-      return new URL(apiEndpoint);
-    } catch (MalformedURLException mue) {
-      throw new IllegalStateException("Improperly formed URL", mue);
+    public static String requestAuth() throws Exception {
+        final String card = "4111111111111111";
+        final String exp_date = "1219";
+        final String amount = "25.00";
+        final String requestBody = "{\"type\":\"AUTH\",\"card\":\"" + card
+                + "\",\"exp-date\":\"" + exp_date + "\",\"amount\":\"" + amount
+                + "\"}";
+        return requestTransaction(requestBody);
     }
-  }
 
-  private static String getResponse(final InputStream inputStream) {
-    final StringBuilder response = new StringBuilder();
-    BufferedReader bufferedReader;
-    try {
-      bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-      String output;
-      while ((output = bufferedReader.readLine()) != null) {
-        response.append(output);
-      }
-    } catch (IOException ioe) {
-      throw new IllegalStateException("Unable to read from endpoint", ioe);
+    public static String requestCapture() throws Exception {
+        final String id = "12345";
+        final String amount = "25.00";
+        final String requestBody = "{\"type\":\"CAPTURE\",\"id\":\"" + id
+                + "\",\"amount\":\"" + amount + "\"}";
+        return requestTransaction(requestBody);
     }
-    return response.toString();
-  }
 
-  private static void sendRequest(final String requestBody, final OutputStream outputStream) {
-    try {
-      outputStream.write(requestBody.getBytes());
-      outputStream.flush();
-    } catch (IOException ioe) {
-      throw new IllegalStateException("Unable to write to endpoint", ioe);
+    public static String requestForcedSale() throws Exception {
+        final String card = "4111111111111111";
+        final String exp_date = "1219";
+        final String amount = "25.00";
+        final String order_id = "12345";
+        final String authorization_code = "APPC10";
+        final String requestBody = "{\"type\":\"FORCED SALE\",\"card\":\""
+                + card + "\",\"exp-date\":\"" + exp_date + "\",\"amount\":\""
+                + amount + "\",\"order-id\":\"" + order_id
+                + "\",\"authorization-code\":\"" + authorization_code + "\"}";
+        return requestTransaction(requestBody);
     }
-  }
 
-  private static void setRequestParamters(final HttpURLConnection httpConnection, String apiKey) throws ProtocolException {
-    httpConnection.setRequestMethod(POST_METHOD);
-    httpConnection.setRequestProperty(CONTENT_TYPE_KEY, APPLICATION_JSON);
-    httpConnection.setRequestProperty(ACCEPT_HEADER_KEY, APPLICATION_JSON);
-    httpConnection.setRequestProperty("api-key", API_KEY);
-  }
+    public static String requestRefund() throws Exception {
+        final String card = "4111111111111111";
+        final String amount = "25.00";
+        final String requestBody = "{\"type\":\"REFUND\",\"card\":\"" + card
+                + "\",\"amount\":\"" + amount + "\"}";
+        return requestTransaction(requestBody);
+    }
+
+    public static String requestVoid() throws Exception {
+        final String id = "12345";
+        final String requestBody = "{\"type\":\"VOID\",\"id\":\"" + id + "\"}";
+        return requestTransaction(requestBody);
+    }
+
+    private static String requestTransaction(String requestBody)
+            throws IOException {
+
+        HttpURLConnection httpConnection = null;
+        try {
+            httpConnection = setupHttpConnection(API_URI);
+            setRequestParamters(httpConnection, API_URI);
+            sendRequest(requestBody, httpConnection.getOutputStream());
+            return getResponse(httpConnection.getInputStream());
+        } catch (IOException e) {
+            return getResponse(httpConnection.getErrorStream());
+        } catch (Exception e) {
+            return getResponse(httpConnection.getErrorStream());
+        } finally {
+            if (httpConnection != null) {
+                httpConnection.disconnect();
+            }
+        }
+    }
+
+    private static HttpURLConnection setupHttpConnection(
+            final String apiEndpoint) {
+        final URL url = createUrl(apiEndpoint);
+        return openHttpConnection(url);
+    }
+
+    private static HttpURLConnection openHttpConnection(final URL url) {
+        try {
+            final HttpURLConnection httpConnection = (HttpURLConnection) url
+                    .openConnection();
+            httpConnection.setDoOutput(OUTPUT_TRUE);
+            return httpConnection;
+        } catch (IOException ioe) {
+            throw new IllegalStateException(
+                    "Unable to open connection with endpoint", ioe);
+        }
+    }
+
+    private static URL createUrl(final String apiEndpoint) {
+        try {
+            return new URL(apiEndpoint);
+        } catch (MalformedURLException mue) {
+            throw new IllegalStateException("Improperly formed URL", mue);
+        }
+    }
+
+    private static String getResponse(final InputStream inputStream) {
+        final StringBuilder response = new StringBuilder();
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(
+                    inputStream));
+            String output;
+            while ((output = bufferedReader.readLine()) != null) {
+                response.append(output);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Unable to read from endpoint", ioe);
+        }
+        return response.toString();
+    }
+
+    private static void sendRequest(final String requestBody,
+            final OutputStream outputStream) {
+        try {
+            outputStream.write(requestBody.getBytes());
+            outputStream.flush();
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Unable to write to endpoint", ioe);
+        }
+    }
+
+    private static void setRequestParamters(
+            final HttpURLConnection httpConnection, String apiKey)
+            throws ProtocolException {
+        httpConnection.setRequestMethod(POST_METHOD);
+        httpConnection.setRequestProperty(CONTENT_TYPE_KEY, APPLICATION_JSON);
+        httpConnection.setRequestProperty(ACCEPT_HEADER_KEY, APPLICATION_JSON);
+        httpConnection.setRequestProperty("api-key", API_KEY);
+    }
 
 }
